b9b9c277c8c89ef45263611e49d410a7da0f4407
Erik Gafni <egafni@gmail.com>
2014-06-06 18:31:49 -0700

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     # Metadata
     name="mdma",
-    version="0.1.3",
+    version="0.1.4",
     description="ipython notebook markdown magic",
     url="",
     author="Erik Gafni",
